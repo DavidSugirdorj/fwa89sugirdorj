030bb7b185d84c8d2918a383b39ed29c30edea64
David Sugirdorj <norane247@gmail.com>
2021-10-27 22:35:56 -0500

    fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydatgit addaRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mydataRouter);
+app.use('/mydata', mydataRouter);
 app.use('/index', usersRouter);
 
 // catch 404 and forward to error handler
